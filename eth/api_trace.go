9ac9cb23b6d49c506e1caef45cb0524c3f5f4c3f
dubuqingfeng <1135326346@qq.com>
2021-07-12 19:13:57 +0800

    初步实现 trace_block

---
+++ eth/api_trace.go
@@ -0,0 +1,500 @@
+// Copyright 2017 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package eth
+
+import (
+	"context"
+	"errors"
+	"encoding/json"
+	"math/big"
+	"sync"
+	"runtime"
+	"fmt"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/consensus"
+	"github.com/ethereum/go-ethereum/consensus/ethash"
+	"github.com/ethereum/go-ethereum/internal/ethapi"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+const (
+	// defaultTraceTimeout is the amount of time a single transaction can execute
+	// by default before being forcefully aborted.
+	defaultTraceTimeout = 5 * time.Second
+
+	// defaultTraceReexec is the number of blocks the tracer is willing to go back
+	// and reexecute to produce missing historical state necessary to run a specific
+	// trace.
+	defaultTraceReexec = uint64(128)
+)
+
+// ParityTrace A trace in the desired format (Parity/OpenEtherum) See: https://Parity.github.io/wiki/JSONRPC-trace-module
+type ParityTrace struct {
+	Action              TraceRewardAction `json:"action"`
+	BlockHash           common.Hash       `json:"blockHash"`
+	BlockNumber         uint64            `json:"blockNumber"`
+	Error               string            `json:"error,omitempty"`
+	Result              interface{}       `json:"result"`
+	Subtraces           int               `json:"subtraces"`
+	TraceAddress        []int             `json:"traceAddress"`
+	TransactionHash     *common.Hash      `json:"transactionHash"`
+	TransactionPosition *uint64           `json:"transactionPosition"`
+	Type                string            `json:"type"`
+}
+
+// TraceRewardAction An Parity formatted trace reward action
+type TraceRewardAction struct {
+	Value      *hexutil.Big    `json:"value,omitempty"`
+	Author     *common.Address `json:"author,omitempty"`
+	RewardType string          `json:"rewardType,omitempty"`
+}
+
+// txTraceResult is the result of a single transaction trace.
+type txTraceResult struct {
+	Result interface{} `json:"result,omitempty"` // Trace results produced by the tracer
+	Error  string      `json:"error,omitempty"`  // Trace failure produced by the tracer
+}
+
+// txTraceContext is the contextual infos about a transaction before it gets run.
+type txTraceContext struct {
+	index int         // Index of the transaction within the block
+	hash  common.Hash // Hash of the transaction
+	block common.Hash // Hash of the block containing the transaction
+}
+
+// txTraceTask represents a single transaction trace task when an entire block
+// is being traced.
+type txTraceTask struct {
+	statedb *state.StateDB // Intermediate state prepped for tracing
+	index   int            // Transaction offset in the block
+}
+
+var (
+	big8  = big.NewInt(8)
+	big32 = big.NewInt(32)
+)
+
+// setConfigTracerToParity forces the Tracer to the Parity one
+func setConfigTracerToParity(config *tracers.TraceConfig) *tracers.TraceConfig {
+	if config == nil {
+		config = &tracers.TraceConfig{}
+	}
+
+	tracer := "callTracerOpenethertum"
+	config.Tracer = &tracer
+	return config
+}
+
+// GetRewards credits the coinbase of the given block with the mining
+// reward. The total reward consists of the static block reward and rewards for
+// included uncles. The coinbase of each uncle block is also rewarded.
+func GetRewards(config *params.ChainConfig, header *types.Header, uncles []*types.Header) (*big.Int, []*big.Int) {
+	// Select the correct block reward based on chain progression
+	blockReward := ethash.FrontierBlockReward
+	if config.IsByzantium(header.Number) {
+		blockReward = ethash.ByzantiumBlockReward
+	}
+	if config.IsConstantinople(header.Number) {
+		blockReward = ethash.ConstantinopleBlockReward
+	}
+	// Accumulate the rewards for the miner and any included uncles
+	uncleRewards := make([]*big.Int, len(uncles))
+	reward := new(big.Int).Set(blockReward)
+	r := new(big.Int)
+	for i, uncle := range uncles {
+		r.Add(uncle.Number, big8)
+		r.Sub(r, header.Number)
+		r.Mul(r, blockReward)
+		r.Div(r, big8)
+
+		ur := new(big.Int).Set(r)
+		uncleRewards[i] = ur
+
+		r.Div(blockReward, big32)
+		reward.Add(reward, r)
+	}
+
+	return reward, uncleRewards
+}
+
+func traceBlockReward(ctx context.Context, eth *Ethereum, block *types.Block, config *tracers.TraceConfig) (*ParityTrace, error) {
+	chainConfig := eth.blockchain.Config()
+	minerReward, _ := GetRewards(chainConfig, block.Header(), block.Uncles())
+
+	coinbase := block.Coinbase()
+
+	tr := &ParityTrace{
+		Type: "reward",
+		Action: TraceRewardAction{
+			Value:      (*hexutil.Big)(minerReward),
+			Author:     &coinbase,
+			RewardType: "block",
+		},
+		TraceAddress: []int{},
+		BlockHash:    block.Hash(),
+		BlockNumber:  block.NumberU64(),
+	}
+
+	return tr, nil
+}
+
+func traceBlockUncleRewards(ctx context.Context, eth *Ethereum, block *types.Block, config *tracers.TraceConfig) ([]*ParityTrace, error) {
+	chainConfig := eth.blockchain.Config()
+	_, uncleRewards := GetRewards(chainConfig, block.Header(), block.Uncles())
+
+	results := make([]*ParityTrace, len(uncleRewards))
+	for i, uncle := range block.Uncles() {
+		if i < len(uncleRewards) {
+			coinbase := uncle.Coinbase
+
+			results[i] = &ParityTrace{
+				Type: "reward",
+				Action: TraceRewardAction{
+					Value:      (*hexutil.Big)(uncleRewards[i]),
+					Author:     &coinbase,
+					RewardType: "uncle",
+				},
+				TraceAddress: []int{},
+				BlockNumber:  block.NumberU64(),
+				BlockHash:    block.Hash(),
+			}
+		}
+	}
+
+	return results, nil
+}
+
+// Block returns the structured logs created during the execution of
+// EVM and returns them as a JSON object.
+// The correct name will be TraceBlockByNumber, though we want to be compatible with Parity trace module.
+func (api *PrivateTraceAPI) Block(ctx context.Context, number rpc.BlockNumber, config *tracers.TraceConfig) ([]interface{}, error) {
+	// Fetch the block that we want to trace
+	var block *types.Block
+
+	switch number {
+	case rpc.PendingBlockNumber:
+		block = api.eth.miner.PendingBlock()
+	case rpc.LatestBlockNumber:
+		block = api.eth.blockchain.CurrentBlock()
+	default:
+		block = api.eth.blockchain.GetBlockByNumber(uint64(number))
+	}
+	// Trace the block if it was found
+	if block == nil {
+		return nil, fmt.Errorf("block #%d not found", number)
+	}
+
+	config = setConfigTracerToParity(config)
+
+	traceResults, err := api.traceBlockByNumber(ctx, api.eth, number, config)
+	if err != nil {
+		return nil, err
+	}
+
+	traceReward, err := traceBlockReward(ctx, api.eth, block, config)
+	if err != nil {
+		return nil, err
+	}
+
+	traceUncleRewards, err := traceBlockUncleRewards(ctx, api.eth, block, config)
+	if err != nil {
+		return nil, err
+	}
+
+	results := []interface{}{}
+
+	for _, result := range traceResults {
+		var tmp []interface{}
+		if err := json.Unmarshal(result.Result.(json.RawMessage), &tmp); err != nil {
+			return nil, err
+		}
+		results = append(results, tmp...)
+	}
+
+	results = append(results, traceReward)
+
+	for _, uncleReward := range traceUncleRewards {
+		results = append(results, uncleReward)
+	}
+
+	return results, nil
+}
+
+// TraceBlockByNumber returns the structured logs created during the execution of
+// EVM and returns them as a JSON object.
+func (api *PrivateTraceAPI) traceBlockByNumber(ctx context.Context, eth *Ethereum, number rpc.BlockNumber, config *tracers.TraceConfig) ([]*txTraceResult, error) {
+	// Fetch the block that we want to trace
+	var block *types.Block
+
+	switch number {
+	case rpc.PendingBlockNumber:
+		block = eth.miner.PendingBlock()
+	case rpc.LatestBlockNumber:
+		block = eth.blockchain.CurrentBlock()
+	default:
+		block = eth.blockchain.GetBlockByNumber(uint64(number))
+	}
+	// Trace the block if it was found
+	if block == nil {
+		return nil, fmt.Errorf("block #%d not found", number)
+	}
+	return api.traceBlock(ctx, eth, block, config)
+}
+
+// blockByNumber is the wrapper of the chain access function offered by the backend.
+// It will return an error if the block is not found.
+func (api *PrivateTraceAPI) blockByNumber(ctx context.Context, number rpc.BlockNumber) (*types.Block, error) {
+	block := api.eth.blockchain.GetBlockByNumber(uint64(number))
+	if block == nil {
+		return nil, fmt.Errorf("block #%d not found", number)
+	}
+	return block, nil
+}
+
+// blockByHash is the wrapper of the chain access function offered by the backend.
+// It will return an error if the block is not found.
+func (api *PrivateTraceAPI) blockByHash(ctx context.Context, hash common.Hash) (*types.Block, error) {
+	block := api.eth.blockchain.GetBlockByHash(hash)
+	if block == nil {
+		return nil, fmt.Errorf("block %s not found", hash.Hex())
+	}
+	return block, nil
+}
+
+// blockByNumberAndHash is the wrapper of the chain access function offered by
+// the backend. It will return an error if the block is not found.
+//
+// Note this function is friendly for the light client which can only retrieve the
+// historical(before the CHT) header/block by number.
+func (api *PrivateTraceAPI) blockByNumberAndHash(ctx context.Context, number rpc.BlockNumber, hash common.Hash) (*types.Block, error) {
+	block, err := api.blockByNumber(ctx, number)
+	if err != nil {
+		return nil, err
+	}
+	if block.Hash() == hash {
+		return block, nil
+	}
+	return api.blockByHash(ctx, hash)
+}
+
+type chainContext struct {
+	api *PrivateTraceAPI
+	ctx context.Context
+}
+
+func (context *chainContext) Engine() consensus.Engine {
+	return context.api.eth.Engine()
+}
+
+func (context *chainContext) GetHeader(hash common.Hash, number uint64) *types.Header {
+	header := context.api.eth.blockchain.GetHeaderByNumber(number)
+	if header.Hash() == hash {
+		return header
+	}
+	header = context.api.eth.blockchain.GetHeaderByHash(hash)
+	return header
+}
+
+// chainContext construts the context reader which is used by the evm for reading
+// the necessary chain context.
+func (api *PrivateTraceAPI) chainContext(ctx context.Context) core.ChainContext {
+	return &chainContext{api: api, ctx: ctx}
+}
+
+// traceBlock configures a new tracer according to the provided configuration, and
+// executes all the transactions contained within. The return value will be one item
+// per transaction, dependent on the requestd tracer.
+func (api *PrivateTraceAPI) traceBlock(ctx context.Context, eth *Ethereum, block *types.Block, config *tracers.TraceConfig) ([]*txTraceResult, error) {
+	if block.NumberU64() == 0 {
+		return nil, errors.New("genesis is not traceable")
+	}
+	parent, err := api.blockByNumberAndHash(ctx, rpc.BlockNumber(block.NumberU64()-1), block.ParentHash())
+	if err != nil {
+		return nil, err
+	}
+	reexec := defaultTraceReexec
+	if config != nil && config.Reexec != nil {
+		reexec = *config.Reexec
+	}
+	statedb, err := api.eth.stateAtBlock(parent, reexec, nil, true)
+	if err != nil {
+		return nil, err
+	}
+	// Execute all the transaction contained within the block concurrently
+	var (
+		signer  = types.MakeSigner(api.eth.blockchain.Config(), block.Number())
+		txs     = block.Transactions()
+		results = make([]*txTraceResult, len(txs))
+
+		pend = new(sync.WaitGroup)
+		jobs = make(chan *txTraceTask, len(txs))
+	)
+	threads := runtime.NumCPU()
+	if threads > len(txs) {
+		threads = len(txs)
+	}
+	blockCtx := core.NewEVMBlockContext(block.Header(), api.chainContext(ctx), nil)
+	blockHash := block.Hash()
+	for th := 0; th < threads; th++ {
+		pend.Add(1)
+		go func() {
+			defer pend.Done()
+			// Fetch and execute the next transaction trace tasks
+			for task := range jobs {
+				msg, _ := txs[task.index].AsMessage(signer, block.BaseFee())
+				txctx := &txTraceContext{
+					index: task.index,
+					hash:  txs[task.index].Hash(),
+					block: blockHash,
+				}
+				res, err := api.traceTx(ctx, msg, txctx, blockCtx, task.statedb, config)
+				if err != nil {
+					results[task.index] = &txTraceResult{Error: err.Error()}
+					continue
+				}
+				results[task.index] = &txTraceResult{Result: res}
+			}
+		}()
+	}
+	// Feed the transactions into the tracers and return
+	var failed error
+	for i, tx := range txs {
+		// Send the trace task over for execution
+		jobs <- &txTraceTask{statedb: statedb.Copy(), index: i}
+
+		// Generate the next state snapshot fast without tracing
+		msg, _ := tx.AsMessage(signer, block.BaseFee())
+		statedb.Prepare(tx.Hash(), block.Hash(), i)
+		vmenv := vm.NewEVM(blockCtx, core.NewEVMTxContext(msg), statedb, api.eth.blockchain.Config(), vm.Config{})
+		if _, err := core.ApplyMessage(vmenv, msg, new(core.GasPool).AddGas(msg.Gas())); err != nil {
+			failed = err
+			break
+		}
+		// Finalize the state so any modifications are written to the trie
+		// Only delete empty objects if EIP158/161 (a.k.a Spurious Dragon) is in effect
+		statedb.Finalise(vmenv.ChainConfig().IsEIP158(block.Number()))
+	}
+	close(jobs)
+	pend.Wait()
+
+	// If execution failed in between, abort
+	if failed != nil {
+		return nil, failed
+	}
+	return results, nil
+}
+
+// traceTx configures a new tracer according to the provided configuration, and
+// executes the given message in the provided environment. The return value will
+// be tracer dependent.
+func (api *PrivateTraceAPI) traceTx(ctx context.Context, message core.Message, txctx *txTraceContext, vmctx vm.BlockContext, statedb *state.StateDB, config *tracers.TraceConfig) (interface{}, error) {
+	// Assemble the structured logger or the JavaScript tracer
+	var (
+		tracer    vm.Tracer
+		err       error
+		txContext = core.NewEVMTxContext(message)
+	)
+	switch {
+	case config != nil && config.Tracer != nil:
+		// Define a meaningful timeout of a single transaction trace
+		timeout := defaultTraceTimeout
+		if config.Timeout != nil {
+			if timeout, err = time.ParseDuration(*config.Timeout); err != nil {
+				return nil, err
+			}
+		}
+		// Constuct the JavaScript tracer to execute with
+		if tracer, err = tracers.New(*config.Tracer, txContext); err != nil {
+			return nil, err
+		}
+		// Handle timeouts and RPC cancellations
+		deadlineCtx, cancel := context.WithTimeout(ctx, timeout)
+		go func() {
+			<-deadlineCtx.Done()
+			if deadlineCtx.Err() == context.DeadlineExceeded {
+				tracer.(*tracers.Tracer).Stop(errors.New("execution timeout"))
+			}
+		}()
+		defer cancel()
+
+	case config == nil:
+		tracer = vm.NewStructLogger(nil)
+
+	default:
+		tracer = vm.NewStructLogger(config.LogConfig)
+	}
+	// Run the transaction with tracing enabled.
+	vmenv := vm.NewEVM(vmctx, txContext, statedb, api.eth.blockchain.Config(), vm.Config{Debug: true, Tracer: tracer, NoBaseFee: true})
+
+	// Call Prepare to clear out the statedb access list
+	statedb.Prepare(txctx.hash, txctx.block, txctx.index)
+
+	result, err := core.ApplyMessage(vmenv, message, new(core.GasPool).AddGas(message.Gas()))
+	if err != nil {
+		return nil, fmt.Errorf("tracing failed: %w", err)
+	}
+
+	// Depending on the tracer type, format and return the output.
+	switch tracer := tracer.(type) {
+	case *vm.StructLogger:
+		// If the result contains a revert reason, return it.
+		returnVal := fmt.Sprintf("%x", result.Return())
+		if len(result.Revert()) > 0 {
+			returnVal = fmt.Sprintf("%x", result.Revert())
+		}
+		return &ethapi.ExecutionResult{
+			Gas:         result.UsedGas,
+			Failed:      result.Failed(),
+			ReturnValue: returnVal,
+			StructLogs:  ethapi.FormatLogs(tracer.StructLogs()),
+		}, nil
+
+	case *tracers.Tracer:
+		return tracer.GetResult()
+
+	default:
+		panic(fmt.Sprintf("bad tracer type %T", tracer))
+	}
+}
+
+// Transaction returns the structured logs created during the execution of EVM
+// and returns them as a JSON object.
+// func (api *PrivateTraceAPI) Transaction(ctx context.Context, hash common.Hash, config *tracers.TraceConfig) (interface{}, error) {
+// 	config = setConfigTracerToParity(config)
+// 	return api.traceTx(ctx, api.eth, hash, config)
+// }
+
+// PrivateTraceAPI is the collection of Ethereum full node APIs exposed over
+// the private trace endpoint.
+type PrivateTraceAPI struct {
+	eth *Ethereum
+}
+
+// NewPrivateTraceAPI creates a new API definition for the full node-related
+// private trace methods of the Ethereum service.
+func NewPrivateTraceAPI(eth *Ethereum) *PrivateTraceAPI {
+	return &PrivateTraceAPI{eth: eth}
+}
